36389f8ac19f31f23c36ef3b2015465c923163d8
Programsko Inzenjerstvo <pi_foi@outlook.com>
2022-05-10 14:16:44 +0200

    Implementirane entitetne klase.

---
+++ teacher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager
+{
+	public class teacher :: Person
+	{
+		public int password { get; set; }
+	}
+}
